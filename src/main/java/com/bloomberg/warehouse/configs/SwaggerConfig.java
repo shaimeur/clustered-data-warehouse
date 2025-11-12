2dba8626322c85250ed2e3803b1fc73c6193f36a
shaimeur <haimeursaad@gmail.com>
2025-11-12 13:35:00 +0100

    feat: Add main Spring Boot application and configs

---
+++ src/main/java/com/bloomberg/warehouse/configs/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.bloomberg.warehouse.configs;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("ClusteredData Warehouse REST API")
+                        .description("Data warehouse for Bloomberg to analyze FX deals."));
+    }
+
+}
